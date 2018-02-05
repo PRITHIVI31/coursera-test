7e9976c45bd34df172ef36fc1202aa62724498af
Prithiviraj <prithivist@gmail.com>
2018-02-05 16:31:09 +0530

    Angular Name Calculator

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 (function()
 {
     'use strict';
-    angular.module('myFirstApp',[])
-    .controller('MyFirstController',function($scope)
+    angular.module('nameCalculator',[])
+    .controller('NameCalculatorController',function($scope)
 {
-$scope.name="Hello";
-$scope.name1="Angular";
-$scope.sayHello=function()
+$scope.name="";
+$scope.totalValue=0;
+$scope.displayNumeric=function()
 {
-    return "Hello" + " " +"Angular!!!!";
-};
-
-});
+    var totalNameValue=calculateValue($scope.name);
+    $scope.totalValue=totalNameValue;
+}
+function calculateValue(string)
+{
+    var total=0;
+    for (let i = 0; i < string.length; i++) {
+          total+=string.charCodeAt(i);
+        }
+        return total;
+}
+$scope.totalValue=calculateValue($scope.name);
+})
 })();
